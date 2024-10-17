85ae2b951b7f70ef8ebafe85f02205fa49bcd6c6
Victor Suarez <zerasul@gmail.com>
2024-10-17 18:10:22 +0200

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,18 @@
+class Calc:
+  def sum(self,a,b):
+    # TODO: Sumar 2 numeros
+    pass
+  def sub(self,a,b):
+    # TODO: Restar 2 numeros
+    pass
+  def mult(self,a,b):
+    #TODO: multiplicar 2 numeros
+    pass
+  def div(self,a,b):
+    #TODO: Dividir 2 numeros. SI b es 0 saltar una excepcion.
+    pass
+  def pow(self,a,b):
+    #TODO: eleviar a con b.
+    pass
+    
+  
